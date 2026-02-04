6a0a1685c84b37017e1da346d4e9e7df67f5a0c8
Gambito404 <gabrielberriosmendoza@gmail.com>
2026-02-04 18:04:44 +0000

    gg

---
+++ resources/views/ToolsTable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ToolsTable\ToolsTable\Components;
+
+use Livewire\Component;
+
+class ToolsTable extends Component
+{
+    public $message = '¡Hola! El paquete de prueba funciona.';
+
+    public function render()
+    {
+        // El componente se puede invocar en cualquier vista de Blade con:
+        // <livewire:tools-table />
+        return view('tools-table::datatable');
+    }
+}
